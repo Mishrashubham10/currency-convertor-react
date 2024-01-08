fc5eb632033ae71036a2c9df524cf87012a9f6ce
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-01-08 08:08:05 +0530

    creates design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div>
-      <h1>Currency Convertor</h1>
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat" style={{backgroundImage: `url(${"https://images.pexels.com/photos/4497591/pexels-photo-4497591.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"})`}}>
+      <h1 className="bg-red-200">Currency Convertor</h1>
     </div>
   )
 }
